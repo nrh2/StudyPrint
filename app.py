147ce0a69f2891b4a71ea5621576fc5c73926dcd
nrh2 <tmtnntk19900811@gmail.com>
2025-08-09 18:53:59 +0900

    ファイル構成変更中

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from top import top_bp
-from narabikae import narabikae_bp
 
 app = Flask(__name__)
 
 # Blueprint登録
 app.register_blueprint(top_bp)
-app.register_blueprint(narabikae_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ top/views.py
@@ -0,0 +1,31 @@
+
+# top.py から全コピーした
+
+# -*- coding: utf-8 -*-
+from flask import render_template, request, redirect, url_for
+from . import top_bp
+
+from constants import (
+    PAGE_TITLE_TOP,SHEET_NAMES
+)
+
+# トップページ処理
+@top_bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # シート名選択
+        selected_sheet = request.form.get('sheet_name')
+
+        # 存在しないシート名が選択された場合の処理
+        if selected_sheet not in SHEET_NAMES:
+            return "不正なシート名が選択されました。", 400
+
+        # Blueprint名+関数名でURL生成
+        return redirect(url_for(f"{selected_sheet}.index"))
+
+    # GETリクエスト時（初期表示）
+    return render_template(
+        'top.html',
+        title=PAGE_TITLE_TOP,
+        sheet_names=SHEET_NAMES
+    )
